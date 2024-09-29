253201c9fa12b650154cd9c34c28ae57d62abc39
IgorYudintsev <kakvospitat@gmail.com>
2024-09-29 09:17:51 +0300

    29.10.2024-9:17-IGOR

---
+++ src/Igor.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Igor = () => {
+    return (
+        <div style={{backgroundColor: 'red'}}>
+            <div>29.10.2024-9:17-IGOR</div>
+        </div>
+    );
+};
