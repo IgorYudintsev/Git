3dc687e46b8924292243901a7cdbfed29eb98095
IgorYudintsev <kakvospitat@gmail.com>
2024-09-29 09:13:54 +0300

    'start'

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>29.10.2024-9:10-MASTER</div>
+    </div>
+  );
+}
+
+export default App;
