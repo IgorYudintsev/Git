bcd0a8d4af333abf1a4be5c5231e37909b3b5f73
George <GPPoskannyy@gmail.com>
2024-09-29 10:18:40 +0300

    29.10.2024-10:15-GEORGY

---
+++ src/Georgy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Georgy = () => {
+    return (
+        <div style={{backgroundColor: 'yellow'}}>
+            <div>29.10.2024-10:15-GEORGY</div>
+        </div>
+    );
+};
