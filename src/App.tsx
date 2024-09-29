76f52e46ac71902e87bd3b380c40fbca1d669166
IgorYudintsev <kakvospitat@gmail.com>
2024-09-29 09:23:22 +0300

    29.10.2024-9:29-MASTER(+Igor)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>29.10.2024-9:10-MASTER</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>29.10.2024-9:10-MASTER</div>
+            <div>29.10.2024-9:29-MASTER(+Igor)</div>
+        </div>
+    );
 }
 
 export default App;
