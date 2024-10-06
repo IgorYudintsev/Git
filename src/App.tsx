05910ed232bba84a82113ce369320fd265f5208e
IgorYudintsev <kakvospitat@gmail.com>
2024-10-06 19:10:41 +0300

    10.10.2024-COUNTER

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Igor} from "./Igor";
 import {Georgy} from "./Georgy";
 
 function App() {
+    
+    const[count,setCount]=useState(1)
+    
     return (
         <div className="App">
             <div>29.10.2024-9:10-MASTER</div>
@@ -11,6 +14,11 @@ function App() {
             <Georgy/>
             <Igor/>
             <div>1.10.2024-23:10-MASTER</div>
+            <h3>{count}</h3>
+
+            <button onClick={()=>setCount(count+1)}>+</button>
+
+
         </div>
     );
 }
